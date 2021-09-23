260a15852a3d2ca5510e12a394ba2252851a0a1d
vitorregisr <vitorregisrr@gmail.com>
2021-09-23 20:29:39 -0300

    Fix og:image content icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,12 @@ function App({ Component, pageProps, messages, router }: AppProps) {
           <link rel="shortcut icon" href="/img/icon-512.png" />
           <link rel="apple-touch-icon" href="/img/icon-512.png" />
           {/* @ts-ignore */}
-          <link rel="og:image" itemProp="image" content="/img/og-img.png" />
+          <link
+            rel="og:image"
+            itemProp="image"
+            // @ts-ignore
+            content="https://www.floresta.tv/img/og-img.png"
+          />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#149A41" />
           <meta name="format-detection" content="telephone=no" />
